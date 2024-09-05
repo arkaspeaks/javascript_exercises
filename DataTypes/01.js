bc4dd37a5f4b0fb52899fcbb91476325ebcb2e9f
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-05 13:03:30 +0000

    js

---
+++ DataTypes/01.js
@@ -0,0 +1,10 @@
+//use comments to describe the perfect data types of the variables
+
+
+
+let length = 16;          // number (int)
+let lastName = "Johnson"; // string
+const x = {
+  firstName: "John",
+  lastName: "Doe"
+};                        // object
